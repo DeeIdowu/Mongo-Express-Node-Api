237cc26c4de9facf38d229c4b3cdbfbc405b0ca3
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2019-03-29 15:18:30 -0400

    edit

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -10,14 +10,14 @@ router.get('/', (req,res,next) => {
 
 //Submitting an order
 router.post('/', (req,res, next) => {
-    res.status.json(201).json({
+    res.status.json(201)({
         message: 'Order created'
     });
 });
 
 //Getting order via ID:
 router.get('/:orderId', (req,res, next) => {
-    res.status.json(200).json({
+    res.status.json(200)({
         message: 'Order details',
         orderId: req.params.orderId
     });
@@ -25,7 +25,7 @@ router.get('/:orderId', (req,res, next) => {
 
 //Deleting:
 router.delete('/:orderId', (req,res, next) => {
-    res.status.json(200).json({
+    res.status.json(200)({
         message: 'Order deleted',
         orderId: req.params.orderId
     });
--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const orderRoutes = require('./api/routes/orders');
 //setting up filter for requests starting with /products and handler of arguements via ProductRoutes file
 //meaning endpoint url requests via /products will be forwards to products.js file
 app.use('/products', productRoutes);
-app.use('/order', orderRoutes);
+app.use('/orders', orderRoutes);
 
 //to export app.js
 module.exports = app;
