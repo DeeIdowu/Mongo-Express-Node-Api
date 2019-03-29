6dbcb1572d17137ef0481a133540788263b56a68
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2019-03-29 15:20:34 -0400

    editalot

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -10,14 +10,14 @@ router.get('/', (req,res,next) => {
 
 //Submitting an order
 router.post('/', (req,res, next) => {
-    res.status.json(201)({
+    res.status(201).json({
         message: 'Order created'
     });
 });
 
 //Getting order via ID:
 router.get('/:orderId', (req,res, next) => {
-    res.status.json(200)({
+    res.status(200).json({
         message: 'Order details',
         orderId: req.params.orderId
     });
